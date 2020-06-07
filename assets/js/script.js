73fb4d83e0af80cbb6425ff57cb5b7480b3a6fad
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-06-06 20:13:24 -0600

    get city weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,33 @@
+var currentWeather = function(data){
+    console.log(typeof(data));
+}
+var getWeather =  function(name){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=03f2177cf8e0cdeb9ef1461248689ebd`;
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            console.log(data)
+            currentWeather(data);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect with server");
+    });
+}
+    
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // get value from input element
     var city = $("#city").val().trim();
     console.log(city);
     if (city) {
-    //getCityId(city);
+    getWeather(city);
     $("#city").val("");
     } else {
     alert("Please enter a city");
