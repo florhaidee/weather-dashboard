48072c1b7550f95ea6d193080d7faad81979dd20
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-06-09 00:53:40 -0600

    display UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,35 @@
-var dataOb = [];
-var getUvIndex = function(url){
-
+var dataOb = []
+var appiKey = '03f2177cf8e0cdeb9ef1461248689ebd'
+//function to display UV index
+var getUvIndex = function(data){
+    aux = data.value
+    console.log(aux);
+    if(aux <=2.99){
+        $("#current-weather").append("<p class='card-text'> UV Index: <span class='low'>" + data.value + "</span><p/>");
+    }else if (aux <=5.99){
+        $("#current-weather").append("<p class='card-text'> UV Index: <span class='moderate'>" + data.value + "</span><p/>");          
+    }else if(aux <=7.99){
+        $("#current-weather").append("<p class='card-text'> UV Index: <span class='high'>" + data.value + "</span><p/>");
+    }else if (aux <=10.99){
+        $("#current-weather").append("<p class='card-text'> UV Index: <span class='very-high'>" + data.value + "</span><p/>");
+    }else if(aux >= 11){
+        $("#current-weather").append("<p class='card-text'> UV Index: <span class='extreme'>" + data.value + "</span><p/>");
+    }
 }
 //Display Current weather
 var currentWeather = function(data, name){
     console.log(data);
     var Name = name[0].toUpperCase() + name.slice(1);
     var day = moment().format("L")
-    var icon = data.weather[0].icon
-    var iconUrl = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+    var icon = data.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
     var temp = $("#temp").text("Temperature: " + data.main.temp + "°F");
     var hum = $("#humidity").text("Humidity: " + data.main.humidity + "%");
     var wind = $("#wind").text("Wind Speed: " + data.wind.speed + " MPH");
-    //var uv = getUvIndex(data.coord.lat, dat.coord.lon);
     $("#current-weather").html(`<h2 class='card-title mycard'>${Name} <span> ${day} <img src=${iconUrl}></span></h2>`)
     $("#current-weather h2").after(temp, hum, wind)
 }
-var getWeather =  function(apiUrl, name){
+/*var getWeather =  function(apiUrl, name){
     fetch(apiUrl)
     .then(function(response) {
         // request was successful
@@ -32,12 +45,50 @@ var getWeather =  function(apiUrl, name){
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to connect with server");
     });
-}  
+}  */
+var getWeather =  function(apiUrl, name){
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          return response.json();
+        } else {
+            alert("Please try again. Error: " + response.statusText);
+        }
+        
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect with server");
+    })
+    .then(function(data) {
+        currentWeather(data, name)
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        console.log(lat, "lat:", typeof(lat))
+        console.log("lon:", typeof(lon));
+        var apiUVurl = 'http://api.openweathermap.org/data/2.5/uvi?appid='+ appiKey +'&lat='+lat+'&lon='+lon;
+        return fetch(apiUVurl);
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        getUvIndex(data)
+    
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        console.log("UV INDEX ERROR");
+        console.log(error,"UV INDEX");
+        alert("Unable to connect with server");
+    });        
+} 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // get value from input element
     var city = $("#city").val().trim();
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=03f2177cf8e0cdeb9ef1461248689ebd`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${appiKey}`;
     console.log(city);
     if (city) {
         getWeather(apiUrl,city);
